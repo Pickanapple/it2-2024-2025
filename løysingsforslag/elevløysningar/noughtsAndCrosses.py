919012427884477f5fc7b7e0221de977698ef55b
pickanapple <thasselgreen@gmail.com>
2024-09-23 16:43:58 +0200

    oppryddning og fikset fortsett

--- løysingsforslag/elevløysningar/noughtsAndCrosses.py
+++ løysingsforslag/elevløysningar/noughtsAndCrosses.py
@@ -1,5 +1,3 @@
-#Laged av Thomas
-
 import os 
 
 board = [
@@ -38,7 +36,7 @@ def turn(player, usedCoordinates):
     while True:         
         try:
             #Sjekker om de er kun tall og mapper dem 
-            coordinates = list(map(int, input("Hvor vil du plassere brikken din? Oppgi 2 tall som x og y koordinat (for eksempel: 0 0) ").split()))
+            coordinates = list(map(int, input("Hvor vil du plassere brikken din? Oppgi 2 tall som x og y koordinat (for eksempel: 2 2) ").split()))
 
             #Sjekker om det kun er 2 tall
             if len(coordinates) == 2:
@@ -133,14 +131,16 @@ if __name__ == "__main__":
     printBoard()
     noughtsAndCrosses()
     while True: 
-        fortsett = input("Vil du fortsette? Y/N ")
+        fortsett = input("Vil du fortsette? Y/N ").upper()
         if fortsett == "Y":
             print("Ny runde")
+            
             board = [
-    [-1,-1,-1],
-    [-1,-1,-1],
-    [-1,-1,-1]
-    ]
+                     [-1,-1,-1],
+                     [-1,-1,-1],
+                     [-1,-1,-1]
+                    ]
+            
             clearScreen()
             noughtsAndCrosses()
         elif fortsett == "N":
